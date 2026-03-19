48ea8c954b239e578440295dcebf0c3480062186
tashaljordan-dev <tashaljordan@gmail.com>
2026-03-18 19:30:12 -0500

    dd

---
+++ vite-project/src/Home.jsx
@@ -0,0 +1,19 @@
+function Home() {
+  return (
+    <div>
+      <h1>Help a Doggy Out!</h1>
+      <p>
+    Welcome to our Help A Doggy Out website! We are dedicated to helping owners of diabetic dogs track blood sugar levels and insulin injections. Browse through our available dogs and consider adopting or donating to support our cause. Together, we can make a difference in the lives of these furry friends!
+      </p>
+    <section style={{ marginTop: "20px" }}>
+        <h2> About this Project </h2>
+        <p> 
+            This project was built using React, Vite, and React Router. It demonstrates basic routing and component structure.
+        </p>
+    </section>
+    </div>
+  );
+}
+
+export default Home;
+
